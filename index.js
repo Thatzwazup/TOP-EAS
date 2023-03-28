2f32c77ecba92b5bd1b344f3cacdb6c8bb97762c
Thatzwazup <51454744+Thatzwazup@users.noreply.github.com>
2023-03-28 19:17:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+let color = "black";
+
+function generateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "lightgrey";
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+
+generateBoard(16);
+
+function changeSize(input) {
+  if (input >= 1 || input <= 64) {
+    generateBoard(input);
+  } else {
+    console.log("Error Size");
+  }
+}
+
+function colorSquare() {
+  if (color === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "lightgrey"));
+}
